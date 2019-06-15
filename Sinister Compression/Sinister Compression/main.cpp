b42141b93c5ae529f057779687a8f6bca6effe23
Harshavardhan K <harshavardhan816@gmail.com>
2019-06-16 02:11:35 +0530

    First commit

---
+++ Sinister Compression/Sinister Compression/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sinister Compression
+//
+//  Created by Harshavardhan K on 16/06/19.
+//  Copyright © 2019 Harshavardhan K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Beginning compression algorithms tutorials!\n";
+    return 0;
+}
